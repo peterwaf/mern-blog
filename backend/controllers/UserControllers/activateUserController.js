df6b2fb3722a502c7948deab4842921a4cae56c8
peter wafula <peterwaf@gmail.com>
2025-02-10 08:47:52 +0300

    update user logic

--- backend/controllers/UserControllers/activateUserController.js
+++ backend/controllers/UserControllers/activateUserController.js
@@ -1,14 +1,25 @@
-// const User = require("../../models/User");
-// const jwt = require("jsonwebtoken");
+const User = require("../../models/User");
+
 const activateUserEmail = async (req, res) => {
     try {
-        const user = res.user; // Use the user found by the middleware
         const userEmail = req.query.email;
-        if (user.email === userEmail) {
-            user.isVerified = true;
-            await user.save();
-            res.status(200).json({ message: "User activated successfully" });
+
+        if (!userEmail) {
+            return res.status(400).json({ error: "Email query parameter is required" });
         }
+
+        // Find the user by email
+        const user = await User.findOne({ email: userEmail });
+
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        // Activate the user
+        user.isVerified = true;
+        await user.save();
+
+        res.status(200).json({ message: "User activated successfully" });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
