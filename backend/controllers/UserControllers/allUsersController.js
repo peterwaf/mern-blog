4f3e48a89d0586eb25f0f0ddb7d7d2d4785c6991
peter wafula <peterwaf@gmail.com>
2025-02-10 08:21:42 +0300

    update displayed users

--- backend/controllers/UserControllers/allUsersController.js
+++ backend/controllers/UserControllers/allUsersController.js
@@ -4,7 +4,18 @@ const jwt = require("jsonwebtoken");
 const allUsers = async (req, res) => {
   try {
     const users = await User.find();
-    res.status(200).json(users);
+    const displayUsers = users.map((user) => {
+      return {
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        uid: user._id,
+        profilePic: user.profilePic ? user.profilePic : "",
+        bio: user.bio ? user.bio : "",
+        isVerified: user.isVerified,
+      };
+    })
+    res.status(200).json(displayUsers);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
